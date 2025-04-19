b91b0b3d54b25d2d16d810892f3ad6a5cd195a13
sparttt3 <axi89341@gmail.com>
2025-04-19 13:32:39 +0200

    Mise a jour

--- assets/js/projet.js
+++ assets/js/projet.js
@@ -1,35 +1,71 @@
 let currentIndex = 0;
+
 const projects = [
     [
-        "assets/imgs/Proxmox.jpg",
-        "assets/imgs/haproxy-keepalived.jpg",
-        "assets/imgs/déckédex.jpg",
-        "assets/imgs/déckédex.jpg"
+        {
+            src: "assets/imgs/Proxmox.jpg",
+            doc: "documentation/documentation_projet_proxmox.pdf"
+        },
+        {
+            src: "assets/imgs/haproxy-keepalived.jpg",
+            doc: "documentation/documentation_projet_proxmox3.pdf"
+        },
+        {
+            src: "assets/imgs/déckédex.jpg",
+            doc: "documentation/documentation_projet_proxmox2.pdf"
+        },
+        {
+            src: "assets/imgs/déckédex.jpg",
+            doc: "documentation/documentation_projet_proxmox1.pdf"
+        }
     ],
     [
-        "assets/imgs/projet5.jpg",
-        "assets/imgs/projet6.jpg",
-        "assets/imgs/projet7.jpg",
-        "assets/imgs/projet8.jpg"
+        {
+            src: "assets/imgs/projet5.jpg",
+            doc: "documentation/documentation_projet5.pdf"
+        },
+        {
+            src: "assets/imgs/projet6.jpg",
+            doc: "documentation/documentation_projet6.pdf"
+        },
+        {
+            src: "assets/imgs/projet7.jpg",
+            doc: "documentation/documentation_projet7.pdf"
+        },
+        {
+            src: "assets/imgs/projet8.jpg",
+            doc: "documentation/documentation_projet8.pdf"
+        }
     ],
     [
-        "assets/imgs/projet9.jpg",
-        "assets/imgs/projet10.jpg",
-        "assets/imgs/projet11.jpg",
-        "assets/imgs/projet12.jpg"
+        {
+            src: "assets/imgs/projet9.jpg",
+            doc: "documentation/documentation_projet9.pdf"
+        },
+        {
+            src: "assets/imgs/projet10.jpg",
+            doc: "documentation/documentation_projet10.pdf"
+        },
+        {
+            src: "assets/imgs/projet11.jpg",
+            doc: "documentation/documentation_projet11.pdf"
+        },
+        {
+            src: "assets/imgs/projet12.jpg",
+            doc: "documentation/documentation_projet12.pdf"
+        }
     ]
 ];
 
 function updateGrid() {
     const grid = document.getElementById("projectGrid");
-    grid.innerHTML = projects[currentIndex].map(imgSrc => {
-        // Extraire le nom du fichier pour le mettre en alt
-        const altText = imgSrc.split('/').pop().split('.')[0].replace(/-/g, ' ');
+    grid.innerHTML = projects[currentIndex].map(project => {
+        const altText = project.src.split('/').pop().split('.')[0].replace(/-/g, ' ');
         return `
             <div class="project">
-                <img src="${imgSrc}" alt="${altText}" class="img-project">
+                <img src="${project.src}" alt="${altText}" class="img-project">
                 <div class="overlay">
-                    <button class="btn btn-primary">Voir la documentation</button>
+                    <a href="${project.doc}" download class="btn btn-primary">Télécharger la documentation</a>
                 </div>
             </div>
         `;
@@ -46,6 +82,5 @@ function prevGrid() {
     updateGrid();
 }
 
-
-// Initialisation de la première grille
+// Initialisation de la grille au chargement
 updateGrid();
